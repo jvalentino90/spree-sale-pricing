f51ed9647680135a4ec9c9acf1a8031656d6de77
Juan Valentino <jvalentino90@gmail.com>
2018-07-03 23:32:06 -0300

    Replace delegate_belongs_to

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Variant.class_eval do
 
-  delegate_belongs_to :default_price, :sale_price, :original_price
+  delegate :sale_price, :original_price, to: :default_price
 
   # TODO also accept a class reference for calculator type instead of only a string
   def put_on_sale(value, calculator_type = "Spree::Calculator::DollarAmountSalePriceCalculator", all_currencies = true, start_at = Time.now, end_at = nil, enabled = true)
@@ -23,11 +23,11 @@ Spree::Variant.class_eval do
   def sale_price_in(currency)
     Spree::Price.new variant_id: self.id, currency: currency, amount: price_in(currency).sale_price
   end
-  
+
   def discount_percent_in(currency)
     price_in(currency).discount_percent
   end
-  
+
   def on_sale_in?(currency)
     price_in(currency).on_sale?
   end
@@ -51,14 +51,14 @@ Spree::Variant.class_eval do
   def stop_sale(all_currencies=true)
     run_on_prices(all_currencies) { |p| p.stop_sale }
   end
-  
+
   private
-   
+
   def run_on_prices(all_currencies, &block)
     if all_currencies && prices.present?
       prices.each { |p| block.call p }
     else
-      block.call default_price  
+      block.call default_price
     end
   end
 end
